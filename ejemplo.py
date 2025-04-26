abb448f8771b530e6b1c050ef441a6cb97e57212
Gustavo <gemp2403@gmail.com>
2025-04-26 11:51:12 -0400

    ejemplo de prepa

--- ejemplo.py
+++ ejemplo.py
@@ -1,2 +1,13 @@
-print("Hello World")
-print("Hola Mundo")
+#Programa que le pida al usuario su nombre y su edad y devuelva un saludo y el año en que nació
+
+"""
+comentario de
+varias lineas"""
+
+año = 2025
+nombre = input("Escribe tu nombre: ")
+edad = int(input("Escribe tu edad: "))
+
+nacimiento = 2025 - edad
+
+print(f"Hola {nombre}, tienes {edad} años y naciste en el año {nacimiento}.")
